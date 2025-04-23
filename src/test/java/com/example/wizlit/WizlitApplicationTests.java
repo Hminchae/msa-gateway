72a06791f164f34466b173146b26a51348273aa8
최수용 <dpfprtus@gmail.com>
2025-04-23 23:11:33 +0900

    환경 구축

---
+++ src/test/java/com/example/wizlit/WizlitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wizlit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WizlitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
